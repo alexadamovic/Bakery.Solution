9cbfe560e4b633f9b7ae26093670134cab6d7c07
Alex Adamovic <alexanderadamovic@gmail.com>
2022-02-25 15:21:55 -0800

    add UserSwitch function to UI to allow for multiple input attempts

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,36 +10,43 @@ namespace Bakery
     {
       Console.WriteLine("\n############################################\n###    ~ Welcome to Pierre's Bakery ~    ### \n############################################\n^ The only bakery with bread AND pastries! ^\n\nWould you like to buy some bread and/or pastries?? (Y/N)");
       string userDecision = Console.ReadLine().ToUpper();
-      switch (userDecision)
+      UserSwitch();
+
+      void UserSwitch()
       {
-        case "Y":
-          Console.WriteLine("Good decision my friend!\nHere is our pricing:\n\nBread - $5 per loaf w/ every 3rd loaf FREE\nPastries - $2 per pastry w/ $1 OFF every 3rd pastry\n(we're so generous, we know...)\n\nHow many loaves of bread would you like to buy?? (please enter a positive, whole number)");
-          int userBreadNumber = int.Parse(Console.ReadLine());
+        switch (userDecision)
+        {
+          case "Y":
+            Console.WriteLine("Good decision my friend!\nHere is our pricing:\n\nBread - $5 per loaf w/ every 3rd loaf FREE\nPastries - $2 per pastry w/ $1 OFF every 3rd pastry\n(we're so generous, we know...)\n\nHow many loaves of bread would you like to buy?? (please enter a positive, whole number)");
+            int userBreadNumber = int.Parse(Console.ReadLine());
 
-          Bread userBread = new Bread(userBreadNumber);
-          int breadFullPrice = userBread.GetPrice();
-          int breadDiscount = userBread.Deals();
+            Bread userBread = new Bread(userBreadNumber);
+            int breadFullPrice = userBread.GetPrice();
+            int breadDiscount = userBread.Deals();
 
-          Console.WriteLine("Okay gotcha! That'll be $" + breadFullPrice + " for the bread without the special deal (discount is applied at checkout after all).\nNow how many pastries would you like to buy?? (please enter a positive, whole number)");
-          int userPastryNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Okay gotcha! That'll be $" + breadFullPrice + " for the bread without the special deal (discount is applied at checkout after all).\nNow how many pastries would you like to buy?? (please enter a positive, whole number)");
+            int userPastryNumber = int.Parse(Console.ReadLine());
 
-          Pastry userPastry = new Pastry(userPastryNumber);
-          int pastryFullPrice = userPastry.GetPrice();
-          int pastryDiscount = userPastry.Deals();
+            Pastry userPastry = new Pastry(userPastryNumber);
+            int pastryFullPrice = userPastry.GetPrice();
+            int pastryDiscount = userPastry.Deals();
 
-          int userTotal = breadFullPrice + pastryFullPrice - breadDiscount - pastryDiscount;
-          int savings = breadDiscount + pastryDiscount;
+            int userTotal = breadFullPrice + pastryFullPrice - breadDiscount - pastryDiscount;
+            int savings = breadDiscount + pastryDiscount;
 
-          Console.WriteLine("Wonderful! That'll be $" + pastryFullPrice + " for the pastries without the special deal (discount is still applied at checkout)\n\nSo let's see here... That's " + userBreadNumber + "x loaves of bread and " + userPastryNumber + "x pastries, which means that your total with the discount is...\n(hold on a sec just gotta do the math real quick...)\n\n...only $" + userTotal + ". WOW!!! Looks like you saved $" + savings +".\n\nThanks for your business and please come again!");
-          break;
-        
-        case "N":
-          Console.WriteLine("Then scram! Window shoppers aren't welcome here at Pierre's Bakery!");
-          break;
+            Console.WriteLine("Wonderful! That'll be $" + pastryFullPrice + " for the pastries without the special deal (discount is still applied at checkout)\n\nSo let's see here... That's " + userBreadNumber + "x loaves of bread and " + userPastryNumber + "x pastries, which means that your total with the discount is...\n(hold on a sec just gotta do the math real quick...)\n\n...only $" + userTotal + ". WOW!!! Looks like you saved $" + savings +".\n\nThanks for your business and please come again!");
+            break;
+          
+          case "N":
+            Console.WriteLine("Then scram! Window shoppers aren't welcome here at Pierre's Bakery!");
+            break;
 
-        default:
-          Console.WriteLine("What??? I don't know what you're saying. We only deal in Y or N for this answer. Come back another day when you learn to follow directions...");
-          break;
+          default:
+            Console.WriteLine("What??? I don't know what you're saying. We only deal in Y or N for this answer. Try again please:");
+            userDecision = Console.ReadLine().ToUpper();
+            UserSwitch();
+            break;
+        }
       }
     }
   }
